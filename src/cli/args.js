12ac8ec2cb0f0b8b7d978f66b9aca25b6130004d
inord777dev <inord777dev@gmail.com>
2022-06-02 14:33:30 +0300

    feat: args complete

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,17 @@
+import { argv } from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+  const result = [];
+  argv.forEach((val, index) => {
+    if (index > 1) {
+      if (index % 2) {
+        result.push(`${result.pop()} is ${val}`);
+      } else {
+        result.push(`${argv[index].replace(/^--/g, '')}`);
+      }
+    }
+  });
+  console.log(result.length > 0 ? result.join(', ') : 'No args' );
 };
+
+parseArgs();
